b01476024b27bc7e83255a4589d97fafe20bf709
olihec <hectorow@dulwich.org.uk>
2021-01-25 21:02:49 +0000

    Added bullet range and reload time as well as powerup class and zombie and bullet collisions

--- InitialCode.py
+++ InitialCode.py
@@ -8,6 +8,7 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 DARK_GREEN = (34,139,34)
+YELLOW = (255,255,0)
  
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 700
@@ -28,6 +29,8 @@ class Zombie(pygame.sprite.Sprite):
         
         self.x_speed = 0
         self.y_speed = 0
+
+        self.health = 100
 
         # variables to make the zombie follow the player
 
@@ -50,7 +53,21 @@ class Zombie(pygame.sprite.Sprite):
         self.x_speed = round(2*(self.x_difference /self.diagonal))
         self.y_speed = round(2*(self.y_difference /self.diagonal))
     
-        
+class Powerup(pygame.sprite.Sprite):
+    # this is the class to give the player powerups
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface([10,10])
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        # assigning random powerup to the sprite
+        powerup_list = ["Shotgun"]
+        x = random.randint(0, len(powerup_list) - 1)
+        self.type = powerup_list[x]
+     
 
 class Tree(pygame.sprite.Sprite):
     # this is the class for our trees in the map
@@ -77,7 +94,11 @@ class Bullet(pygame.sprite.Sprite):
         self.rect.y = y
         self.damage = 10
 
+        self.range = 40
         
+
+        # variable to check how long bullet has been shot for (range)
+        self.timer = 0
         # mouse position when bullet is shot
 
         pos = pygame.mouse.get_pos()
@@ -96,6 +117,9 @@ class Bullet(pygame.sprite.Sprite):
         self.diagonal = math.sqrt((self.y_difference ** 2) + (self.x_difference ** 2))
 
         if gun == "Shotgun":
+            self.damage = 20
+            self.range = 10
+
             x = random.randint(pos[0] - round(self.diagonal / 2) , round(pos[0] + self.diagonal / 2))
             y = random.randint(pos[1] - round(self.diagonal / 2), round(pos[1] + self.diagonal / 2))
 
@@ -106,6 +130,7 @@ class Bullet(pygame.sprite.Sprite):
 
     def update(self):
         
+        self.timer = self.timer + 1
         # maths to make the bullet fire in the direction of the mouse
         
         self.rect.x = self.rect.x + round(10* (self.x_difference /self.diagonal))
@@ -138,16 +163,19 @@ class Player(pygame.sprite.Sprite):
         self.score = 0
 
         # variable to track which gun player has
-        self.gun = "Shotgun"
+        self.gun = "Pistol"
+
+        self.reload_time = 20
+        self.shot_timer = 0
+        self.gun_reloaded = True
 
         # image for the hearts
         self.heart_image = pygame.image.load("heart.png").convert()
         self.heart_image.set_colorkey(BLACK)
 
-        font = pygame.font.SysFont('Calibri', 25, True, False)
+        self.font = pygame.font.SysFont('Calibri', 25, True, False)
  
-        self.text = font.render("Score: " + str(self.score),True,WHITE)
-
+        
     def move(self, x_speed, y_speed):
         # method to change the speed variables of the player
         
@@ -155,7 +183,11 @@ class Player(pygame.sprite.Sprite):
         self.y_speed = self.y_speed + y_speed
     def update(self):
         
-        
+        self.text = self.font.render("Score: " + str(self.score),True,WHITE)
+
+
+        if self.gun_reloaded == False:
+            self.shot_timer = self.shot_timer + 1
 
 
         # making the sprite rotate
@@ -203,6 +235,7 @@ class Game(object):
         self.bullet_list = pygame.sprite.Group()
         self.tree_list = pygame.sprite.Group()
         self.zombie_list = pygame.sprite.Group()
+        self.powerup_list = pygame.sprite.Group()
 
         # timer in game used for bullets and zombies
         self.timer = 0
@@ -255,14 +288,18 @@ class Game(object):
                 self.map[y][x] = 1
                 count = count + 1
     
-
+        # variable to keep track of where there arn't trees
+        self.empty_spaces = []
         for j in range(35):
             for i in range(50):
                 if self.map[j][i] == 1:
                     self.tree = Tree(i*20,j*20)
                     self.all_sprites_list.add(self.tree)
                     self.tree_list.add(self.tree)
+                else:
+                    self.empty_spaces.append((j,i))
         # Create the player
+
         self.player = Player()
         self.all_sprites_list.add(self.player)
 
@@ -270,6 +307,11 @@ class Game(object):
         self.zombie = Zombie(600,600)
         self.all_sprites_list.add(self.zombie)
         self.zombie_list.add(self.zombie)
+
+        # Create powerup
+        self.powerup = Powerup(600,600)
+        self.all_sprites_list.add(self.powerup)
+        self.powerup_list.add(self.powerup)
     def process_events(self):
         # Process all of the events. Return a "True" if we need
         #    to close the window. 
@@ -279,16 +321,18 @@ class Game(object):
                 return True
             # bullet shooting
             if event.type == pygame.MOUSEBUTTONDOWN:
-
-                if self.player.gun == "Pistol":
-                    self.bullet = Bullet(self.player.player_centre_x, self.player.player_centre_y, self.player.gun)
-                    self.all_sprites_list.add(self.bullet)
-                    self.bullet_list.add(self.bullet)
-                elif self.player.gun == "Shotgun":
-                    for i in range(5):
+                if self.player.gun_reloaded == True:
+                    self.player.gun_reloaded = False
+                    self.player.shot_timer = 0
+                    if self.player.gun == "Pistol":
                         self.bullet = Bullet(self.player.player_centre_x, self.player.player_centre_y, self.player.gun)
                         self.all_sprites_list.add(self.bullet)
                         self.bullet_list.add(self.bullet)
+                    elif self.player.gun == "Shotgun":
+                        for i in range(5):
+                            self.bullet = Bullet(self.player.player_centre_x, self.player.player_centre_y, self.player.gun)
+                            self.all_sprites_list.add(self.bullet)
+                            self.bullet_list.add(self.bullet)
             # Player movement code
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
@@ -328,23 +372,45 @@ class Game(object):
 
         self.all_sprites_list.update()
         
-        # removing bullets from sprite list if they collide with
+        # check is reload time is finished
+        if self.player.reload_time == self.player.shot_timer:
+            self.player.gun_reloaded = True
+
+        # removing bullets from sprite list if they collide or reach range limit
         for self.bullet in self.bullet_list:
-            trees_hit = pygame.sprite.spritecollide(self.bullet, self.tree_list, False)
-            for self.tree in trees_hit:
-                # checking if tree is a side tree
+            # remove bullets if their range is reached
+            if self.bullet.timer == self.bullet.range:
                 self.bullet_list.remove(self.bullet)
                 self.all_sprites_list.remove(self.bullet)
-                if not(self.tree.rect.x == 0 or self.tree.rect.x == 980 or self.tree.rect.y == 0 or self.tree.rect.y == 680):
-                    
-                    self.tree.health = self.tree.health - self.bullet.damage
+        
+            else:
+                # collide with zombie
+                zombie_hit = pygame.sprite.spritecollide(self.bullet, self.zombie_list, False) 
+                for self.zombie in zombie_hit:
+                    self.zombie.health = self.zombie.health - self.bullet.damage
                     self.bullet_list.remove(self.bullet)
                     self.all_sprites_list.remove(self.bullet)
                     if self.tree.health < 1:
-                        # remove tree if its health goes below 0
+                            # remove zombie if its health goes below 0
+                            self.player.score = self.player.score + 10
+                            self.zombie_list.remove(self.zombie)
+                            self.all_sprites_list.remove(self.zombie)
+                # collide with tree
+                trees_hit = pygame.sprite.spritecollide(self.bullet, self.tree_list, False)
+                for self.tree in trees_hit:
+                    # checking if tree is a side tree
+                    self.bullet_list.remove(self.bullet)
+                    self.all_sprites_list.remove(self.bullet)
+                    if not(self.tree.rect.x == 0 or self.tree.rect.x == 980 or self.tree.rect.y == 0 or self.tree.rect.y == 680):
                         
-                        self.tree_list.remove(self.tree)
-                        self.all_sprites_list.remove(self.tree)
+                        self.tree.health = self.tree.health - self.bullet.damage
+                        self.bullet_list.remove(self.bullet)
+                        self.all_sprites_list.remove(self.bullet)
+                        if self.tree.health < 1:
+                            # remove tree if its health goes below 0
+                            self.player.score = self.player.score + 5
+                            self.tree_list.remove(self.tree)
+                            self.all_sprites_list.remove(self.tree)
                     
         
         zombie_hit = pygame.sprite.spritecollide(self.player, self.zombie_list, True) 
@@ -353,12 +419,19 @@ class Game(object):
             self.zombie_list.remove(self.zombie)
             self.all_sprites_list.remove(self.zombie)
 
-
+        # check for collision with powerup
+        powerup_hit = pygame.sprite.spritecollide(self.player, self.powerup_list, True) 
+        for self.powerup in powerup_hit:
+            if self.powerup.type == "Shotgun":
+                self.player.gun = self.powerup.type
+                self.player.reload_time = 40
+            self.powerup_list.remove(self.powerup)
+            self.all_sprites_list.remove(self.powerup)
         
         # update sprite position
         self.player.rect.x = self.player.rect.x + self.player.x_speed
         
-
+    
         # making it so player can't pass through tree
         tree_hit_list = pygame.sprite.spritecollide(self.player, self.tree_list, False)
         for self.tree in tree_hit_list:
@@ -408,7 +481,7 @@ class Game(object):
         # drawing the hearts and score of the player
         for i in range(self.player.lives):
             screen.blit(self.player.heart_image, [i * 20 + 900, 2])
-        screen.blit(self.player.text, [800, 2])
+        screen.blit(self.player.text, [780, 2])
  
         pygame.display.flip()
  
